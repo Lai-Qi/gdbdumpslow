488d43a6b7331190f7513c51e630bf61dc9872eb
laiqi <laiqi12345@gmail.com>
2024-09-11 20:09:59 +0800

    Switch to reading the slow query log in slurp mode to avoid empty lines in the SQL.

--- gdbdumpslow.pl
+++ gdbdumpslow.pl
@@ -5,19 +5,18 @@ use Getopt::Long;
 # at, al, and ar are the corresponding averages
 my %opt = (
     s => 'at',
-    h => '*',
 );
 
 GetOptions(\%opt,
     'v|verbose+',# verbose
-    'help+',	# write usage info
-    'd|debug+',	# debug
-    's=s',	# what to sort by (at,ap,ag,af,c,t,p,g,f)
-    'r!',	# reverse the sort order (largest last instead of first)
-    't=i',	# just show the top n queries
-    'a!',	# don't abstract all numbers to N and strings to 'S'
-    'n=i',	# abstract numbers with at least n digits within names
-    'g=s',	# grep: only consider stmts that include this string
+    'help+', # write usage info
+    'd|debug+', # debug
+    's=s', # what to sort by (at,ap,ag,af,c,t,p,g,f)
+    'r!', # reverse the sort order (largest last instead of first)
+    't=i', # just show the top n queries
+    'a!', # don't abstract all numbers to N and strings to 'S'
+    'n=i', # abstract numbers with at least n digits within names
+    'g=s', # grep: only consider stmts that include this string
 ) or usage("bad option");
 
 $opt{'help'} and usage();
@@ -30,31 +29,36 @@ unless (@ARGV) {
     }
 }
 
-warn "\nReading slow query log from @ARGV\n";
-my %stmt;
-$/ = "";		# read entire statements using paragraph mode
-while ( defined($_ = <>) ) {
-    warn "[[$_]]\n" if $opt{d};	# show raw paragraph being read
 
-    my ($port,$host,$user,$t,$sql,$p,$g,$f) = s/.*Port\[(\d+)\]Session\[\d*\]TransSerial\[\d*\]LinkIP\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]LinkPort\[\d+\]UserName\[(\S+)\]ProxyName\[\S+\]ClusterName\[\S+\]Database\[\S*\]TotalExecTime\[(\d+)us\]BeginTs\[\S+\]EndTs\[\S+\]SQL\[(.*)\]\ndigest\[\S+\]\nMsgToExecTime\[\S+\]ParserSQLTime\[(\d+)us,\S+\]PlanTreeCreateTime\[\S+\]GetGTIDTime\[(\d+)us,\S+\]GetAutoIncValueTime\[\S*\]FreeGtidTime\[(\d*)us,\S+\]PlanTreeExecTime\[\S+\].*//s ? ($1,$2,$3,$4,$5,$6,$7,$8) : ('','','','','','','','');
-    warn "{{$sql}}\n\n" if $opt{d};	# show processed statement string
+my $whole_file = do {local $/; <> };
+warn "\nReading slow query log from @ARGV\n";
+
+my $entry_separator = '(?m)(?=^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}:\d{3}\])';
+my @entries = split /$entry_separator/,$whole_file;
+
+my %stmt;
+foreach (@entries) {
+    warn "[[$_]]\n" if $opt{d}; # show raw paragraph being read
+
+    my ($port,$host,$user,$t,$sql,$p,$g,$f) = s/.*Port\[(\d+)\]Session\[\d*\]TransSerial\[\d*\]LinkIP\[(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\]LinkPort\[\d+\]UserName\[(\S+)\]ProxyName\[\S+\]ClusterName\[\S+\]Database\[\S+\]TotalExecTime\[(\d+)us\]BeginTs\[\S+\]EndTs\[\S+\]SQL\[(.*)\]\ndigest\[\S+\]\nMsgToExecTime\[\S+\]ParserSQLTime\[(\d+)us,\S+\]PlanTreeCreateTime\[\S+\]GetGTIDTime\[(\d+)us,\S+\]GetAutoIncValueTime\[\S*\]FreeGtidTime\[(\d*)us,\S+\]PlanTreeExecTime\[\S+\].*//s ? ($1,$2,$3,$4,$5,$6,$7,$8) : ('','','','','','','','');
+    warn "{{$sql}}\n\n" if $opt{d}; # show processed statement string
 
 
     next if $opt{g} and $sql !~ /$opt{g}/io;
 
     unless ($opt{a}) {
-	$sql =~ s/\b\d+\b/N/g;
-	$sql =~ s/\b0x[0-9A-Fa-f]+\b/N/g;
+ $sql =~ s/\b\d+\b/N/g;
+ $sql =~ s/\b0x[0-9A-Fa-f]+\b/N/g;
     $sql =~ s/''/'S'/g;
     $sql =~ s/""/"S"/g;
     $sql =~ s/(\\')//g;
     $sql =~ s/(\\")//g;
     $sql =~ s/'[^']+'/'S'/g;
     $sql =~ s/"[^"]+"/"S"/g;
-	# -n=8: turn log_20001231 into log_NNNNNNNN
-	$sql =~ s/([a-z_]+)(\d{$opt{n},})/$1.('N' x length($2))/ieg if $opt{n};
-	# abbreviate massive "in (...)" statements and similar
-	$sql =~ s!(([NS],){100,})!sprintf("$2,{repeated %d times}",length($1)/2)!eg;
+ # -n=8: turn log_20001231 into log_NNNNNNNN
+ $sql =~ s/([a-z_]+)(\d{$opt{n},})/$1.('N' x length($2))/ieg if $opt{n};
+ # abbreviate massive "in (...)" statements and similar
+ $sql =~ s!(([NS],){100,})!sprintf("$2,{repeated %d times}",length($1)/2)!eg;
     }
 
     my $s = $stmt{$sql} ||= { users=>{}, hosts=>{} };
@@ -65,7 +69,7 @@ while ( defined($_ = <>) ) {
     $s->{f} += $f;
     $s->{hosts}->{$host}++ if $host;
     $s->{users}->{$user}++ if $user;
-    warn "{{$sql}}\n\n" if $opt{d};	# show processed statement string
+    warn "{{$sql}}\n\n" if $opt{d}; # show processed statement string
 }
 
 foreach (keys %stmt) {
@@ -89,7 +93,7 @@ foreach (@sorted) {
     my @hosts = keys %{$v->{hosts}};
     my $host  = (@hosts==1) ? $hosts[0] : sprintf "%dhosts",scalar @hosts;
     printf "Count: %d  Time=%.0fus (%dus)  ParserSQLTime=%.0fus (%dus)  GetGTIDTime=%.0fus (%dus)  FreeGtidTime=%.0fus (%dus)  $user\@$host\n%s\n\n",
-	    $c, $at,$t, $ap,$p,$ag,$g,$af,$f,$_;
+     $c, $at,$t, $ap,$p,$ag,$g,$af,$f,$_;
 }
 
 sub usage {
@@ -111,7 +115,7 @@ Parse and summarize the MySQL slow query log. Options are
                 ag: average GetGTIDTime time
                 af: average FreeGtidTime time
                  c: count
-                 t: query time  
+                 t: query time
                  p: parse sql time
                  g: getgtidtime
                  f: freegtidtime
